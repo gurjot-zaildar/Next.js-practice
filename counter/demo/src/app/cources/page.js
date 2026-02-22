aadca77cbe789cfc8ae0f8ef6aa6bdf6e971c5d9
gurjot-zaildar <signingames115@gmail.com>
2026-02-22 11:43:52 +0530

    adding image or layout.js

--- counter/demo/src/app/cources/page.js
+++ counter/demo/src/app/cources/page.js
@@ -1,11 +1,15 @@
+"use client"
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import image from "../../../public/dragon.jpg"
 
 const page = () => {
   return (
     <div>
       cources page
       <Link href={"/cources/react"}>go to react</Link>
+      <Image src={image} />
     </div>
   )
 }
---
+++ counter/demo/src/app/products/layout.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const layout = ({children}) => {
+
+  return (
+    <div>
+        <div className='bg-red-500 flex gap-10'>
+            <h1>sale is live</h1>
+            <h1>sale is live</h1>
+            <h1>sale is live</h1>
+            <h1>sale is live</h1>
+            <h1>sale is live</h1>
+            <h1>sale is live</h1>
+            <h1>sale is live</h1>
+            <h1>sale is live</h1>
+            <h1>sale is live</h1>
+            <h1>sale is live</h1>
+            <h1>sale is live</h1>
+            <h1>sale is live</h1>
+            <h1>sale is live</h1>
+        </div>
+      {children}
+    </div>
+  )
+}
+
+export default layout
+
